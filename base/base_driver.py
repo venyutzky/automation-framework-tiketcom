3d12e71903bf89d41fb3b71f022676d334944eaa
venyutzky <venyutzky08@gmail.com>
2023-02-09 22:30:08 +0700

    efficiency code

--- base/base_driver.py
+++ base/base_driver.py
@@ -1,5 +1,6 @@
 import time
-
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 class BaseDriver():
     
@@ -14,3 +15,13 @@ class BaseDriver():
             new_height = self.driver.execute_script("return document.body.scrollHeight")
                            
         time.sleep(4)
+        
+    def wait_for_presence_of_all_elements_located(self, locator_type, locator):
+        wait = WebDriverWait(self.driver, 10)
+        list_of_elements = wait.until(EC.presence_of_all_elements_located((locator_type, locator)))
+        return list_of_elements        
+    
+    def wait_until_element_is_clickable(self, locator_type, locator):
+        wait = WebDriverWait(self.driver, 10)
+        element = wait.until(EC.element_to_be_clickable((locator_type, locator)))
+        return element
--- pages/FlightSearchResultPage.py
+++ pages/FlightSearchResultPage.py
@@ -5,19 +5,17 @@ from automation_framework_tiketcom.base.base_driver import BaseDriver
 
 class FlightSearchResultPage(BaseDriver):
     
-    def __init__(self, driver, wait):
+    def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
-        self.wait = wait
     
     def click_popup(self):
         self.driver.find_element(By.XPATH, "//div[@class='comp-info-box']//div[@class='v3-btn v3-btn__blue list-horizontal__middle btn-action']").click()
         time.sleep(1)
         
     def filter_flight(self):
-        # self.driver.find_element(By.XPATH, "//div[@class='wrapper-flight-list'])//div[@class='col-xs-6 relative']//div[@class='row']//span[@class='text-marketing-airline']")
         time.sleep(1)
-        all_airplanes = self.wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='section-box-content']/div/div[@class='wrapper-flight-list']/div[@class='row relative']/div[@class='col-xs-6 relative']/div[@class='row']/span[@class='text-marketing-airline']")))
+        all_airplanes = self.wait_for_presence_of_all_elements_located(By.XPATH, "//div[@class='section-box-content']/div/div[@class='wrapper-flight-list']/div[@class='row relative']/div[@class='col-xs-6 relative']/div[@class='row']/span[@class='text-marketing-airline']")
         print(len(all_airplanes))
         for airplane in all_airplanes:
             print(airplane.text)
